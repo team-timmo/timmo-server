2ef297eeb0221a2438e0e85e69cc31e8beeeecde
soo5717 <soo5717@sookmyung.ac.kr>
2023-03-06 22:14:37 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/timmo/TimmoServerApplication.java
@@ -0,0 +1,13 @@
+package com.timmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimmoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimmoServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/timmo/TimmoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimmoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
